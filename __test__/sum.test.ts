d912f2445c69307aab55121b042a2204dc4aed24
dunghenry <trandungksnb6930082001>
2022-12-09 18:32:14 +0700

    create bug

--- __test__/sum.test.ts
+++ __test__/sum.test.ts
@@ -1,13 +1,13 @@
 import { sum } from '../src/sum';
 
-describe('sum module 1', () => {
-    it('add 1 + 2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
-    });
-});
-
-// describe('sum module 2', () => {
-//     test('adds 1 + 2 to equal 3', () => {
-//         expect(sum(1, 2)).toBe(4);
+// describe('sum module 1', () => {
+//     it('add 1 + 2 to equal 3', () => {
+//         expect(sum(1, 2)).toBe(3);
 //     });
 // });
+
+describe('sum module 2', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(4);
+    });
+});
